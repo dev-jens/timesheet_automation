50b9b11d0259e95a7e0a6a2c73cc17d36daafcff
dev-jens <baetenjens07@gmail.com>
2024-01-20 23:27:00 +0100

    cli url fix

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,22 @@
 import express, { Express } from 'express';
 import { defaultConfig } from './config/default.config';
-// import { bodyParser } from 'bodyParser'
 import router from './routes/index';
-
 
 const PORT = defaultConfig.PORT || 3000;
 const app: Express = express();
 
 
 //middleware
-// app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use('/api', router);
 
-
-
 //routes
 app.use('/api', router);
 
 app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}/api`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
 
 
--- src/services/timesheetService.ts
+++ src/services/timesheetService.ts
@@ -1,27 +1,32 @@
 import axios from "axios";
 import { defaultConfig } from "../config/default.config";
+import { log } from "console";
 
 const API_URL = defaultConfig.API_URL;
 
 
 const fillTimesheetIn = async (year: number, month: number, accessToken: string, userID: string) => {
-
-  const firstDayOfMonth: Date = new Date(year, month - 1, 1);
-  const lastDayOfMonth: Date = new Date(year, month, 0);
- 
+  let firstDayOfMonth: Date = new Date(year, month - 1, 1);
+  let lastDayOfMonth: Date = new Date(year, month, 0);
+  log(firstDayOfMonth);
+  log(lastDayOfMonth);
 
   for (let currentDay = new Date(firstDayOfMonth); currentDay <= lastDayOfMonth; currentDay.setDate(currentDay.getDate() + 1)) {
     const dayOfWeek = currentDay.getDay();
     const formattedDate = currentDay.toISOString().split('T')[0];
 
     if (dayOfWeek >= 1 && dayOfWeek <= 5) {
-      if (dayOfWeek == 3) 
-        await postWorkedHoursAtHome(formattedDate, accessToken, userID);
-      else
-        await postWorkedHoursAtClient(formattedDate, accessToken, userID);
+      if (dayOfWeek === 3) {
+        log(formattedDate);
+        // await postWorkedHoursAtHome(formattedDate, accessToken, userID);
+      } else {
+        log(formattedDate);
+        // await postWorkedHoursAtClient(formattedDate, accessToken, userID);
+      }
     }
   }
 };
+
 
 
 const postWorkedHoursAtClient = async (date: String, accessToken: string, userID: string) => {
@@ -37,8 +42,6 @@ const postWorkedHoursAtClient = async (date: String, accessToken: string, userID
 };
 
 const postWorkedHoursAtHome = async (date: String, accessToken: string, userID: string) => {
-
-  console.log(getHeaders(accessToken));
   return await axios.post(getTimehseerUrl(userID), getObjPresatieAtHome(date), {headers: { Authorization: `${accessToken}`,}})
     .then((response) => {
       return {response :  response.data};
@@ -52,7 +55,7 @@ const postWorkedHoursAtHome = async (date: String, accessToken: string, userID: 
 
 const getTimehseerUrl = (userID: string)  => `${API_URL}/timeoff/employees/${userID}/requests`;
 const getHeaders = (accessToken : string) => ({
-  headers: {'Authorization': `Bearer ${accessToken}` },
+  headers: { Authorization : `${accessToken}` },
 });
 
 
